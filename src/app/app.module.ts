ba562f7d563ac843637f7b7f5239447610797f84
RaMoVa <ramon.molina.v9@gmail.com>
2022-02-17 21:09:59 -0600

    Maquetado web con Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { BodyComponent } from './componentes/body/body.component';
+import { BodyinfoComponent } from './componentes/bodyinfo/bodyinfo.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    BodyinfoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
